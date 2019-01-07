9cd12d2a1667762b51722650cab01a248a0b7453
Rich Lewis <richlewis42@users.noreply.github.com>
2019-01-07 22:04:10 +0000

    refactor(processor): remove most properties
    We no longer track the properties, as they don't maintain state
    after instantiation reliably.
    
    i.e. changing the style doesn't transparently change the citation style
    as this would require making a new Engine, losing the registry data.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import citeproc from 'citeproc'
 
-export default class Manager {
-  constructor({ items, style, locale, format = 'html' }) {
-    Object.assign(this, { items, style, locale, format })
-  }
-
-  get itemLookup() {
-    if (this._itemLookup) return this._itemLookup
-    else {
-      this._itemLookup = {}
-      this.items.forEach(item => (this._itemLookup[item.id] = item))
-      return this._itemLookup
+export default class Processor {
+  constructor({ items, style, locale, format = 'text' }) {
+    const itemLookup = {}
+    items.forEach(item => (itemLookup[item.id] = item))
+    const sys = {
+      retrieveItem: item => itemLookup[item],
+      retrieveLocale: () => locale
     }
-  }
-
-  get engine() {
-    if (this._engine) return this._engine
-    else {
-      this._engine = new citeproc.Engine(this, this.style)
-      this._engine.setOutputFormat(this.format)
-      return this._engine
-    }
+    this.engine = new citeproc.Engine(sys, style)
+    this.format = format
   }
 
   get format() {
@@ -30,14 +19,6 @@ export default class Manager {
   set format(fmt) {
     this.engine.setOutputFormat(fmt)
     this._format = fmt
-  }
-
-  retrieveItem(item) {
-    return this.itemLookup[item]
-  }
-
-  retrieveLocale() {
-    return this.locale
   }
 
   citeInText(citation) {
@@ -71,7 +52,7 @@ export default class Manager {
     return result[1][0][1]
   }
 
-  nocite(citeIds) {
+  noCite(citeIds) {
     this.engine.updateUncitedItems(citeIds)
   }
 
